6f2c9e305fa6aab2470f2fcb734adafeba677641
SandoDirga <81424237+SandoDirga@users.noreply.github.com>
2021-07-30 15:36:13 -0700

    Minggu 1-5

--- Minggu 1-5/storage/framework/views/6fd56497d96d72f34bb84b6843ef7563a340cc46.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<title>gambar routing</title>
-</head>
-<body>
-<center>
-<h3>Daftar Menu <b>"KEDAI KOPI PERJOEANGAN"</b></h3>
-<img src="<?php echo e(('img/gambar1.jpg')); ?>" width='500px' height='auto'>
-</center>
-</body><?php /**PATH C:\xampp\htdocs\E32201197_Mirzando\BelajarLaravel\resources\views/gambar.blade.php ENDPATH**/ ?>
