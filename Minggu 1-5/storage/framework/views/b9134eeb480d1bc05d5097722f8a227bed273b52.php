2c6e4b4ec52609039dbc28049bb3fae50f982f71
SandoDirga <81424237+SandoDirga@users.noreply.github.com>
2021-07-30 15:32:16 -0700

    Minggu 1-5

---
+++ Minggu 1-5/storage/framework/views/b9134eeb480d1bc05d5097722f8a227bed273b52.php
@@ -0,0 +1,18 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Home Page</h1>
+            <p class="lead">This is the Home Page</p>
+        </div>
+        <p>Nama : <?php echo e($nama); ?></p>
+        <p>Mata Kuliah</p>
+        <ul>
+            <?php $__currentLoopData = $matkul; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mk): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <li><?php echo e($mk); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\E32201197_Mirzando\BelajarLaravel\resources\views/Sando.blade.php ENDPATH**/ ?>
